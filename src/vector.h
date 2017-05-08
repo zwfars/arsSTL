d2419e8061227df72bd7302be9cae0bc86af019f
zwfars <wfzhu@mail.ustc.edu.cn>
2017-05-08 15:43:08 +0800

    大致快要完成

--- src/vector.h
+++ src/vector.h
@@ -6,7 +6,7 @@
 #include<allocators>
 
 namespace arsSTL {
-	template <class T, class Allocator = std::allocator<T> > 
+	template <class T, class Allocator = std::allocator<T> >
 	class vector {
 	public:
 		using reference = T&;
@@ -48,7 +48,7 @@ namespace arsSTL {
 		//// iterators:
 		iterator                begin() noexcept { return element; }
 		const_iterator          begin() const noexcept { return element; }
-		iterator                end() noexcept { return first_free;}
+		iterator                end() noexcept { return first_free; }
 		const_iterator          end() const noexcept { return first_free; }
 
 		//reverse_iterator        rbegin() noexcept;
@@ -75,42 +75,41 @@ namespace arsSTL {
 		reference       operator[](size_type n) { return *(begin() + n); }
 		const_reference operator[](size_type n) const { return *(cbegin() + n); }
 		reference       at(size_type n) { return *(begin() + n); }
-		const_reference at(size_type n) const {return *(cbegin() + n);}
+		const_reference at(size_type n) const { return *(cbegin() + n); }
 		reference       front() { return *begin(); }
 		const_reference front() const { return *cbegin(); }
 		reference       back() { return *(end() - 1); }
 		const_reference back() const { return *(cend() - 1); }
-	
+
 		////data access
 		//T*       data() noexcept;
 		//const T* data() const noexcept;
-	
+
 		//// modifiers:
-		template <class... Args> void emplace_back(Args&&... args) { emp_aux(end(), (args)...); }
-		void push_back(const T& x) { emp_aux(end(), x); }
-		void push_back(T&& x) { emp_aux(end(), std::forward<T&&>(x)); }
-		//void pop_back();
-	
+		template <class... Args> void emplace_back(Args&&... args) { emp_aux((args)...); }
+		void push_back(const T& x) { emp_aux(x); }
+		void push_back(T&& x) { emp_aux(std::forward<T>(x)); }
+		void pop_back() { alloc.destroy(--first_free); }
+
 		template <class... Args> iterator emplace(const_iterator position, Args&&... args);
-		iterator insert(const_iterator position, const T& x) { emplace(position, x); }
-		iterator insert(const_iterator position, T&& x) { emplace(position, std::forward<T&&>(x)); }
-		//iterator insert(const_iterator position, size_type n, const T& x);
-		//template <class InputIterator>
-		//iterator insert(const_iterator position, InputIterator first,
-		//	InputIterator last);
-		//iterator insert(const_iterator position, initializer_list<T>);
-	
+		iterator insert(const_iterator position, const T& x) { return emplace(position, x); }
+		iterator insert(const_iterator position, T&& x) { emplace(position, std::forward<T>(x)); }
+		iterator insert(const_iterator position, size_type n, const T& x);
+		template <class InputIterator>
+		iterator insert(const_iterator position, InputIterator first,InputIterator last);
+		iterator insert(const_iterator position, std::initializer_list<T> x) { return insert(position, x.begin(), x.end()); }
+
 		iterator erase(const_iterator position);
 		iterator erase(const_iterator first, const_iterator last);
 		void     swap(vector<T, Allocator>&);
-		void     clear() noexcept {erase(begin(),end())};
+		void     clear() noexcept { erase(begin(), end()) };
 
-    // == and !=
+		// == and !=
 	public:
 		template<class T, class Allocator>
-		friend bool operator == (const vector<T, Allocator>& , const vector<T, Allocator>& );
+		friend bool operator == (const vector<T, Allocator>&, const vector<T, Allocator>&);
 		template<class T, class Allocator>
-		friend bool operator != (const vector<T, Allocator>& , const vector<T, Allocator>& );
+		friend bool operator != (const vector<T, Allocator>&, const vector<T, Allocator>&);
 
 		// some helper functions
 	private:
@@ -120,8 +119,15 @@ namespace arsSTL {
 		void vec_aux(InputIterator first, InputIterator last, std::true_type);
 		void vec_free();
 		template <class... Args>
-		void emp_aux(iterator tempos, Args&&... args);
-		//void ins_aux(iterator, const T&);
+		void emp_aux(Args&&... args);
+		void rotate_aux(iterator low, iterator mid, iterator high);
+		void reverse_aux(iterator low, iterator high);
+		
+
+		template <class InputIterator>
+		iterator ins_aux(const_iterator position,InputIterator first, InputIterator last, std::false_type);
+		template <class InputIterator>
+		iterator ins_aux(const_iterator position,InputIterator first, InputIterator last, std::true_type);
 
 
 	private:
@@ -132,39 +138,39 @@ namespace arsSTL {
 	};
 
 	//construct function
-	template<typename T,typename Allocator>
+	template<typename T, typename Allocator>
 	vector<T, Allocator>::vector(const Allocator& a) :alloc{ a }, element(nullptr), first_free(nullptr), cap(nullptr){}
-     
+
 	template<typename T, typename Allocator>
 	vector<T, Allocator>::vector(size_type n) {
 		element = alloc.allocate(n);
 		uninitialized_fill_n(element, n, T());
 		cap = first_free = element + n;
 	}
-	
+
 	template<typename T, typename Allocator>
-	vector<T,Allocator>::vector(size_type n, const T& value, const Allocator& a): alloc(a){
+	vector<T, Allocator>::vector(size_type n, const T& value, const Allocator& a) : alloc(a) {
 		element = alloc.allocate(n);
 		auto tem = element;
 		for (int i = 0; i != n; i++)
-			alloc.construct(tem++,value);
+			alloc.construct(tem++, value);
 		first_free = cap = tem;
 	}
 
-	template<typename T,typename Allocator>
+	template<typename T, typename Allocator>
 	template <class InputIterator>
 	vector<T, Allocator>::vector(InputIterator first, InputIterator last) {
 		vec_aux(first, last, typename std::is_integral<InputIterator>::type());
 	}
 
-	template<typename T,typename Allocator>
-	vector<T, Allocator>::vector(const vector<T, Allocator>& x){
+	template<typename T, typename Allocator>
+	vector<T, Allocator>::vector(const vector<T, Allocator>& x) {
 		element = alloc.allocate(x.size());
 		cap = first_free = uninitialized_copy(x.begin(), x.end(), element);
 	}
 
 	template<typename T, typename Allocator>
-	vector<T, Allocator>::vector(vector&& x):element(x.element),first_free(x.first_free),cap(x.cap){
+	vector<T, Allocator>::vector(vector&& x) :element(x.element), first_free(x.first_free), cap(x.cap) {
 		x.first_free = x.element = x.cap = nullptr;
 	}
 
@@ -175,13 +181,13 @@ namespace arsSTL {
 	}
 
 	template<typename T, typename Allocator>
-	vector<T, Allocator>::vector(vector&& x, const Allocator& a) :alloc(a),element(x.element),first_free(x.first_free),cap(x.cap) {
+	vector<T, Allocator>::vector(vector&& x, const Allocator& a) :alloc(a), element(x.element), first_free(x.first_free), cap(x.cap) {
 		x.element = x.cap = x.first_free = nullptr;
 
 	}
 
 	template<typename T, typename Allocator>
-	vector<T, Allocator>::vector(std::initializer_list<T> x, const Allocator& a):alloc(a){
+	vector<T, Allocator>::vector(std::initializer_list<T> x, const Allocator& a) :alloc(a) {
 		element = alloc.allocate(x.size());
 		first_free = cap = uninitialized_copy(x.begin(), x.end(), element);
 	}
@@ -196,7 +202,7 @@ namespace arsSTL {
 
 
 	// operator=
-	template<typename T,typename Allocator>
+	template<typename T, typename Allocator>
 	vector<T, Allocator>& vector<T, Allocator>::operator=(const vector<T, Allocator>& x) {
 		auto tem = alloc.allocate(x.size());
 		auto temcap = uninitialized_copy(x.begin(), x.end(), tem);
@@ -220,7 +226,7 @@ namespace arsSTL {
 	}
 
 	template<typename T, typename Allocator>
-	vector<T,Allocator>& vector<T, Allocator>::operator=(std::initializer_list<T> x) {
+	vector<T, Allocator>& vector<T, Allocator>::operator=(std::initializer_list<T> x) {
 		vec_free();
 		element = alloc.allocate(x.size());
 		first_free = cap = uninitialized_copy(x.begin(), x.end(), element);
@@ -229,11 +235,11 @@ namespace arsSTL {
 
 
 	// assign function
-	template<typename T,typename Allocator>
+	template<typename T, typename Allocator>
 	template <class InputIterator>
 	void vector<T, Allocator>::assign(InputIterator first, InputIterator last) {
 		vec_free();
-		vec_aux(first, last,typename std::is_integral<InputIterator>::type());
+		vec_aux(first, last, typename std::is_integral<InputIterator>::type());
 		                    
 	}
 
@@ -254,18 +260,18 @@ namespace arsSTL {
 
 	// erase function
 	template<typename T, typename Allocator>
-	typename vector<T,Allocator>::iterator vector<T,Allocator>::erase(const_iterator position){
+	typename vector<T, Allocator>::iterator vector<T, Allocator>::erase(const_iterator position) {
 		iterator tem = (iterator)position;
 		uninitialized_copy(tem, first_free, tem);
 		--first_free;
 		alloc.destroy(first_free);
 		return tem;
 	}
-	template<typename T,typename Allocator>
+	template<typename T, typename Allocator>
 	typename vector<T, Allocator>::iterator vector<T, Allocator>::erase(const_iterator first, const_iterator last) {
-		iterator temfirst = (iterator) first;
+		iterator temfirst = (iterator)first;
 		iterator temlast = (iterator)last;
-		iterator tem = uninitialized_copy(temlast, first_free,temfirst);
+		iterator tem = uninitialized_copy(temlast, first_free, temfirst);
 		for (auto p = end(); p != temfirst;)
 			alloc.destroy(--p);
 		first_free = tem;
@@ -282,13 +288,43 @@ namespace arsSTL {
 
 
 	//emplace and insert function
-	template<typename T,typename Allocator>
-	template <class... Args> 
+	template<typename T, typename Allocator>
+	template <class... Args>
 	typename vector<T, Allocator>::iterator vector<T, Allocator>::emplace(const_iterator position, Args&&... args) {
-		iterator tempos = (iterator)position;
-		emp_aux(tempos, (args)...);
-		return tempos;
+		size_type off = position - begin();
+		emp_aux((args)...);
+		rotate_aux(begin() + off, end() - 1, end() - 1);
+		return begin() + off;
 	}
+
+	template<typename T, typename Allocator>
+	typename vector<T, Allocator>::iterator vector<T, Allocator>::insert(const_iterator position, size_type n, const T& x) {
+		difference_type off = position - begin();
+		if (cap - end() >= n) {
+			uninitialized_fill_n(end(),n,x);
+			rotate_aux(begin() + off, end(), end() + n - 1);
+			first_free += n;
+		}
+		else {
+			size_type len = size() + max(size(), n);
+			iterator temelement = alloc.allocate(len);
+			iterator cur = uninitialized_copy(begin(), end(), temelement);
+			uninitialized_fill_n(cur, n, x);
+			rotate_aux(temelement + off, cur, cur + n - 1);
+			iterator temfirst_free = temelement + size() + n;
+			vec_free();
+			element = temelement;
+			first_free = temfirst_free;
+			cap = first_free + len;
+		}
+		return begin() + off;
+	}
+
+	template<typename T, typename Allocator>
+	template <class InputIterator>
+	typename vector<T, Allocator>::iterator vector<T, Allocator>::insert(const_iterator position, InputIterator first, InputIterator last) {
+		return ins_aux(position, first, last, typename std::is_integral<InputIterator>::type());
+	}                                        
 
 
 
@@ -333,7 +369,7 @@ namespace arsSTL {
 		first_free = cap = element + first;
 	}
 
-	template<typename T,typename Allocator>
+	template<typename T, typename Allocator>
 	void vector<T, Allocator>::vec_free() {
 		if (element) {
 			for (auto p = first_free; p != element;)
@@ -342,27 +378,96 @@ namespace arsSTL {
 		}
 	}
 
+
+	// push the element in the back;
 	template<typename T, typename Allocator>
 	template <class... Args>
-	void vector<T, Allocator>::emp_aux(iterator tempos, Args&&... args) {
+	void vector<T, Allocator>::emp_aux(Args&&... args) { 
 		if (end() < cap) {
-			uninitialized_copy(tempos, end(), tempos + 1);
-			alloc.construct(tempos, std::forward<Args>(args)...);
-			first_free++;
+			alloc.construct(first_free++, (args)...);
 		}
 		else {
 			size_type temsize = size() ? 2 * size() : 1;
 			iterator temelement = alloc.allocate(temsize);
-			iterator cur = uninitialized_copy(element, tempos, temelement);
-			alloc.construct(cur, std::forward<Args>(args)...);
-			iterator temfirst_free = uninitialized_copy(tempos, end(), cur + 1);
+			iterator temfirst_free = uninitialized_copy(begin(), end(), temelement);
+			alloc.construct(temfirst_free++, (args)...);
 			vec_free();
 			element = temelement;
 			first_free = temfirst_free;
-			cap = temsize + element;
+			cap = element + temsize;
 		}
 	}
 
+	
+	template<typename T, typename Allocator>
+	void vector<T, Allocator>::reverse_aux(iterator low, iterator high) {
+		while (low < high) {
+			std::swap(*low, *high);
+			low++;
+			high--;
+		}
+	}
+
+	template<typename T,typename Allocator>
+	void vector<T, Allocator>::rotate_aux(iterator low, iterator mid, iterator high) {
+		reverse_aux(low, mid - 1);
+		reverse_aux(mid, high);
+		reverse_aux(low, high);
+	}
+
+	template<typename T,typename Allocator>
+	template <class InputIterator>
+	typename vector<T, Allocator>::iterator vector<T, Allocator>::ins_aux(const_iterator position, InputIterator first, InputIterator last, std::true_type) {
+		difference_type off = position - begin();
+		if (cap - end() >= first) {
+			uninitialized_fill_n(end(), first, last);
+			rotate_aux(begin() + off, end(), end() + first - 1);
+			first_free += first;
+		}
+		else {
+			size_type len = size() > first ? size() : first;
+			len += size();
+			iterator temelement = alloc.allocate(len);
+			iterator cur = uninitialized_copy(begin(), end(), temelement);
+			uninitialized_fill_n(cur, first, last);
+			rotate_aux(temelement + off, cur, cur + first - 1);
+			iterator temfirst_free = temelement + size() + first;
+			vec_free();
+			element = temelement;
+			first_free = temfirst_free;
+			cap = first_free + len;
+		}
+		return begin() + off;
+	}
+
+
+	template<typename T, typename Allocator>
+	template <class InputIterator>
+	typename vector<T, Allocator>::iterator vector<T, Allocator>::ins_aux(const_iterator position, InputIterator first, InputIterator last, std::false_type) {
+		difference_type off = position - begin();
+		difference_type n = last - first;
+		if (cap - end() >= n) {
+			uninitialized_copy(first, last, end());
+			rotate_aux(begin()+off,end(),end()+n-1);
+			first_free += n;
+		}
+		else {
+			size_type len = size() > off ? size() : off;
+			len += size();
+			iterator temelement = alloc.allocate(len);
+			iterator cur = uninitialized_copy(begin(), end(), temelement);
+			uninitialized_copy(first, last, cur);
+			rotate_aux(temelement + off, cur, cur + n - 1);
+			iterator temfirst_free = temelement + size() + n;
+			vec_free();
+			element = temelement;
+			first_free = temfirst_free;
+			cap = first_free + len;
+		}
+		return begin() + off;
+
+			
+	}
 }
 
 
