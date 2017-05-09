7761288161077b3c7cea583b5a4d979c08b8ebfc
zwfars <wfzhu@mail.ustc.edu.cn>
2017-05-09 23:27:38 +0800

    Create iterator.h

---
+++ src/iterator.h
@@ -0,0 +1,55 @@
+#ifndef ITERATOR_H
+#define ITERATOR_H
+
+namespace arsSTL {
+	// Iterator tags
+	struct input_iterator_tag {};
+	struct output_iterator_tag {};
+	struct forward_iterator_tag :public input_iterator_tag {};
+	struct bidirectional_iterator_tag :public forward_iterator_tag {};
+	struct random_access_iterator_tag :public bidirectional_iterator_tag {};
+
+	//class iterator
+	template<typename category,typename T,typename Distance = ptrdiff_t,typename Pointer = T*,typename Reference = T&>
+	struct iterator {
+		using value_type = T;
+		using difference_type = Distance;
+		using pointer = Pointer;
+		using reference = Reference;
+		using iterator_category = category;
+	};
+
+	//class iterator_traits
+	template<typename Iterator>
+	struct iterator_traits{
+		using value_type = typename Iterator::value_type;
+		using differenct_type = typename Iterator::difference_type;
+		using pointer = typename Iterator::Pointer;
+		using reference = typename Iterator::Reference;
+		using iterator_category = typename Iterator::category;
+	};
+
+	// iterator_traits--- pointer
+	template<typename T>
+	struct iterator_traits<T*>{
+		using value_type = T;
+		using difference_type = ptrdiff_t;
+		using pointer = T*;
+		using reference = T&;
+		using iterator_category = random_access_iterator_tag;
+	};
+
+	//iterator_traits --- const pointer
+	template<typename T>
+	struct iterator_traits<const T*> {
+		using value_type = T;
+		using difference_type = ptrdiff_t;
+		using pointer = const T*;
+		using reference = const T&;
+		using iterator_category = random_access_iterator_tag;
+	};	
+
+}
+
+
+#endif
