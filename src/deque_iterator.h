1bf5a913f0e3e0ec0d70b1f9d78b48c7ca1eccc7
zwfars <wfzhu@mail.ustc.edu.cn>
2017-06-19 09:54:45 +0800

    Update deque_iterator.h

--- src/deque_iterator.h
+++ src/deque_iterator.h
@@ -14,10 +14,8 @@ namespace arsSTL {
 		using size_type = size_t;
 		using map_pointer = T**;
 		using self = deque_iterator;
-    //
-		size_type chunk_sz() {
-			return 16 / sizeof(value_type) > 1 ? 16 / sizeof(value_type) : size_type(1);
-		}
+	
+    
 	//operator functions
 		self& operator++() {
 			if (cur == last) {
@@ -28,11 +26,13 @@ namespace arsSTL {
 				++cur;
 			return *this;
 		}
+		
 		self operator++(int) {
 			self old = *this;
 			++*this;
 			return old;
 		}
+		
 		self& operator--() {
 			if (cur == first) {
 				set_node(parent - 1);
@@ -42,12 +42,46 @@ namespace arsSTL {
 				--cur;
 			return *this;
 		}
-		self& operator--(int) {
+		
+		self operator--(int) {
 			self old = *this;
 			--*this;
 			return old;
 		}
-	
+
+		self& operator+=(difference_type n) {
+			if (n >= 0){
+				size_type total = cur - first + n;
+				set_node(parent + total / chunk_sz(sizeof(T)));
+				cur = first + total % chunk_sz(sizeof(T));
+			}
+			else {
+				size_type total = -n - (cur - first);
+				set_node(parent - total / chunk_sz(sizeof(T)));
+				cur = last - total % chunk_sz(sizeof(T));
+			}
+			return *this;
+		}
+
+		self& operator-= (difference_type n) {
+			return *this += (-n);
+		}
+
+		self operator+(difference_type n) {
+			self tem = *this;
+			tem += n;
+			return tem;
+		}
+
+		self operator-(difference_type n) {
+			self tem = *this;
+			tem -= n;
+			return tem;
+		}
+
+
+
+		reference operator*() {return *cur;}
 
 
 	//some auxiliary functions
@@ -64,7 +98,18 @@ namespace arsSTL {
 	void deque_iterator<T, Refer, Ptr>::set_node(map_pointer other) {
 		parent = other;
 		first = *parent;
-		last = first + chunk_sz();
+		last = first + chunk_sz(sizeof(T));
+	}
+	
+	// some non-member functions
+	size_t chunk_sz(size_t sz) {
+		return 16 / sz > 1 ? 16 / sz : size_t(1);
+	}
+
+	template<typename T,typename Ref,typename Ptr>
+	typename deque_iterator<T,Ref,Ptr>::difference_type operator-(const deque_iterator<T,Ref,Ptr>& lhs, const deque_iterator<T,Ref,Ptr>& rhs) {
+		return (lhs.parent - rhs.parent - 1)*chunk_sz(sizeof(T)) +
+			(rhs.last - rhs.cur ) + (lhs.cur - lhs.first);
 	}
 }
 
