f1f3035732e96f627efdd70f31bd45b58124a650
zwfars <wfzhu@mail.ustc.edu.cn>
2017-07-05 11:59:06 +0800

    Update queue.h

--- src/queue.h
+++ src/queue.h
@@ -15,7 +15,7 @@ namespace arsSTL {
 		Container c;
 	public:
 		explicit queue(const Container& x) :c(x) {}
-		explicit queue(Container&& x = Container()) :c(x) {}
+		explicit queue(Container&& x = Container()) :c(std::forward<Container>(x)) {}
 		template <class Alloc> explicit queue(const Alloc& a) :c(a) {}
 		template <class Alloc> queue(const Container& x, const Alloc& a) : c(x, a) {}
 		template <class Alloc> queue(Container&& x, const Alloc& a) : c(std::move(x), a) {}
@@ -57,12 +57,12 @@ namespace arsSTL {
 			swap(c, q.c);
 		}
 		// aux funtion
-    //note that const is important
 		Container get_container() const {
 			return c;
 		}
 	};
 
+	// some non-member function of queue
 	template <class T, class Container>
 	bool operator==(const queue<T, Container>& x, const queue<T, Container>& y) {
 		return x.get_container() == y.get_container();
@@ -93,6 +93,70 @@ namespace arsSTL {
 	void swap(queue<T, Container>& x, queue<T, Container>& y) noexcept(noexcept(x.swap(y))) {
 		x.swap(y);
 	}
+
+	template <class T,class Container = vector<T>,
+	class Compare = std::less<typename Container::value_type> >
+	class priority_queue {
+	public:
+		typedef typename Container::value_type value_type;
+		typedef typename Container::reference reference;
+		typedef typename Container::const_reference const_reference;
+		typedef typename Container::size_type size_type;
+		typedef Container container_type;
+	protected:
+		Container c;
+		Compare comp;
+	public:
+		priority_queue(const Compare& x, const Container& cx) :comp(x), c(cx) {}
+		explicit priority_queue(const Compare& x = Compare(), Container&& cx = Container()) :
+			c(std::forward<Container>(cx)), comp(x) {}
+		template <class InputIterator>
+		priority_queue(InputIterator first, InputIterator last, const Compare& x, const Container& cx) :
+			comp(x), c(cx) {
+			c.clear();
+			c.insert(first, last);
+		}
+
+		template <class InputIterator>
+		priority_queue(InputIterator first, InputIterator last,const Compare& x = Compare(), Container&&cx = Container()):
+			comp(x), c(std::forward<Container>cx) {
+			c.clear();
+			c.insert(first, last);
+		}
+
+		template <class Alloc> 
+		explicit priority_queue(const Alloc& a) :c(a) {}
+		template <class Alloc> 
+		priority_queue(const Compare& x, const Alloc& a) : comp(x), c(a) {}
+		template <class Alloc> 
+		priority_queue(const Compare& x, const Container& cx, const Alloc&a) : comp(x), c(cx, a) {}
+		template <class Alloc> 
+		priority_queue(const Compare& x, Container&& cx, const Alloc& a) : comp(x), c(std::forward<Container>(cx), a);
+		template <class Alloc> 
+		priority_queue(const priority_queue& x, const Alloc& a) :comp(x.comp), c(x.c, a) {}
+		template <class Alloc> 
+		priority_queue(priority_queue&& x, const Alloc&a) :comp(x.comp), c(std::move(x.c), a) {}
+
+		bool empty() const {
+			return c.empty();
+		}
+		size_type size() const {
+			return c.size();
+		}
+		const_reference top() const {
+			return c.front();
+		}
+		void push(const value_type& x);
+		void push(value_type&& x);
+		template <class... Args> void emplace(Args&&... args);
+		void pop();
+		void swap(priority_queue& q) noexcept(noexcept(swap(c, q.c))
+			&& noexcept(swap(comp, q.comp))) {
+			using std::swap;
+			swap(c, q.c);
+			swap(comp, q.comp);
+		}
+	};
 }
 
 #endif
